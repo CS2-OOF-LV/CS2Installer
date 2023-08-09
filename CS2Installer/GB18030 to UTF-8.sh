a434941e1363c26ccfbd7d96502f226aa05efa7b
4ndyDufresne <you@example.com>
2023-08-09 21:34:45 +0800

    sh fix

--- CS2Installer/GB18030 to UTF-8.sh
+++ CS2Installer/GB18030 to UTF-8.sh
@@ -1,2 +1,2 @@
 # 将源码编码从GB18030转换为UTF-8
-find . -type f \( -name "*.h" -o -name "*.cpp" \) -exec sh -c 'iconv -f GB18030 -t UTF-8 "{}" > "{}.utf-8" && mv "{}.utf8" "{}"' \;
+find . -type f \( -name "*.h" -o -name "*.cpp" \) -exec sh -c 'iconv -f GB18030 -t UTF-8 "{}" > "{}.utf8" && mv "{}.utf8" "{}"' \;
